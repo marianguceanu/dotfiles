e02fd15c9a79100bd69cf3bf2deca411843efeae
Marian Guceanu <marian.guceanu@yahoo.com>
2026-04-20 10:14:20 +0300

    simple life again, default theming because it will most likely always be there

--- .wezterm.lua
+++ .wezterm.lua
@@ -11,10 +11,12 @@ end
 local function scheme_for_appearance(appearance)
 	if appearance:find("Dark") then
 		-- return "Builtin Dark"
-		return "Catppuccin Mocha"
+		-- return "Catppuccin Mocha"
+		return "Atelierseaside (dark) (terminal.sexy)"
 	else
 		-- return "Builtin Light"
-		return "Catppuccin Latte"
+		-- return "Catppuccin Latte"
+		return "Atelierseaside (light) (terminal.sexy)"
 	end
 end
 config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
